3b08a66bcf5dfc4071624a0fad864058350285bf
Alan <alan.wei@live.com>
2022-03-18 21:05:49 +0800

    feat: 增加IP接口

--- Controllers/ToolController.cs
+++ Controllers/ToolController.cs
@@ -1,8 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AzureAlanWei.Controllers
@@ -11,16 +10,19 @@ namespace AzureAlanWei.Controllers
   [ApiController]
   public class ToolController : ControllerBase
   {
-    public async Task<String> qrCode(String text)
-    {
-      return "ok";
-    }
     public async Task<HttpResponseMessage> download(String url)
     {
       HttpClient client = new HttpClient();
-      HttpResponseMessage response = await client.GetAsync(url) ;
+      HttpResponseMessage response = await client.GetAsync(url);
       Console.WriteLine($"code: ${response.StatusCode}");
       return response;
+    }
+    [Route("ip")]
+    public String ip()
+    {
+      String remoteIp = this.Request.HttpContext.Connection.RemoteIpAddress.ToString();
+      String remotePort = this.Request.HttpContext.Connection.RemotePort.ToString();
+      return $"{remoteIp}:{remotePort}";
     }
   }
 }
